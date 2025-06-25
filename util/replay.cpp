3a54b18fb2dccba3f0740e46e6b80f2b4c696bf0
Bo Gan <ganboing@gmail.com>
2025-06-25 00:21:30 -0700

    util/replay: Use logger interface to compress repeated lines

--- util/replay.cpp
+++ util/replay.cpp
@@ -27,6 +27,7 @@ extern "C" {
 #include "inst.h"
 #include "sym.h"
 #include "opts-def.h"
+#include "logger.h"
 #include "misc.h"
 
 #define DEFAULT_BUFFER_SIZE 4096
@@ -70,30 +71,30 @@ static void help(const char *argv0) {
           argv0, DEFAULT_BUFFER_SIZE);
 }
 
-#define FMT_TIME_OFFSET "\n[%" PRIu64 "] +%zu "
+#define FMT_TIME_OFFSET "[%" PRIu64 "] +%zu "
 
 map<shared_ptr<obj_file>, map<string, sym_server> > sym_srvs;
 unordered_map<uint64_t, shared_ptr<rv_inst_block> > insts;
 
-static void print_label(shared_ptr<memory_view> vm, FILE *fp, uint64_t addr,
+static void print_label(shared_ptr<memory_view> vm, logger& l, uint64_t addr,
                         const string **last_func) {
     auto [func, module, vma] = vm->query_label(addr);
     if (!func)
         return;
     if (*last_func != func)
-        fprintf(fp, " %s", func->c_str());
+        l.format(" %s", func->c_str());
     else if (module)
-        fprintf(fp, " %*s", int(func->size()), "");
+        l.format(" %*s", int(func->size()), "");
     *last_func = func;
     if (module)
-        fprintf(fp, "; %s", module->c_str());
+        l.format("; %s", module->c_str());
 }
 
-static void print_sym(shared_ptr<memory_view> vm, FILE *fp, uint64_t addr,
+static void print_sym(shared_ptr<memory_view> vm, logger& l, uint64_t addr,
                       const string **last_func) {
     auto [obj, section, vma] = vm->query_sym(addr);
     if (!obj) {
-        print_label(vm, fp, addr, last_func);
+        print_label(vm, l, addr, last_func);
         return;
     }
     auto& srvs = sym_srvs[obj];
@@ -104,9 +105,9 @@ static void print_sym(shared_ptr<memory_view> vm, FILE *fp, uint64_t addr,
     auto answer = it->second.query(vma);
     auto *filename = answer.filename;
     if (*last_func != answer.func)
-        fprintf(fp, " %s", answer.func->c_str());
+        l.format(" %s", answer.func->c_str());
     else if (filename)
-        fprintf(fp, " %*s", int(answer.func->size()), "");
+        l.format(" %*s", int(answer.func->size()), "");
     *last_func = answer.func;
     if (!filename)
         return;
@@ -114,24 +115,20 @@ static void print_sym(shared_ptr<memory_view> vm, FILE *fp, uint64_t addr,
     auto it_fn = find(filename->rbegin(), filename->rend(), '/');
     if (it_fn != filename->rend())
         short_fn = string_view(it_fn.base(), filename->end());
-    fprintf(fp, "; %.*s:%lu%s",
+    l.format("; %.*s:%lu%s",
             int(short_fn.size()), short_fn.data(), answer.lineno,
             answer.note ? answer.note->c_str() : "");
 }
 
-static int align_print(int *printed, FILE *fp, int rc) {
-    if (rc < 0)
-        return rc;
-    if (rc < *printed)
-        rc = fprintf(fp, "%*s", *printed - rc, "");
+static void align_print(size_t *max, logger &l, size_t printed) {
+    if (printed < *max)
+        l.format("%*s", (int)(*max - printed), "");
     else
-        *printed = rc;
-    if (rc < 0)
-        return rc;
-    return *printed;
+        *max = printed;
 }
 
 static void replay(shared_ptr<memory_view> vm, nexusrv_msg_decoder *msg_decoder, FILE *fp) {
+    logger l(fp);
     nexusrv_trace_decoder trace_decoder = {};
     int32_t rc = nexusrv_trace_decoder_init(&trace_decoder, msg_decoder);
     if (rc < 0)
@@ -141,7 +138,7 @@ static void replay(shared_ptr<memory_view> vm, nexusrv_msg_decoder *msg_decoder,
     uint64_t last_time = 0;
     optional<uint64_t> lastip;
     const string *last_func = nullptr;
-    int addr_printed = 0, inst_printed = 0;
+    size_t addr_printed = 0, inst_printed = 0;
     for (;;) {
         nexusrv_msg msg;
         nexusrv_trace_indirect indir;
@@ -156,11 +153,12 @@ static void replay(shared_ptr<memory_view> vm, nexusrv_msg_decoder *msg_decoder,
                   str_nexus_error(-rc));
         if (rc > 0) {
             lastip.emplace(sync.addr);
-            fprintf(fp,FMT_TIME_OFFSET " SYNC %u to 0x%" PRIx64,
+            l.newline();
+            l.format(FMT_TIME_OFFSET " SYNC %u to 0x%" PRIx64,
                     nexusrv_trace_time(&trace_decoder),
                     nexusrv_msg_decoder_offset(msg_decoder),
                     sync.sync, *lastip);
-            print_sym(vm, fp, *lastip, &last_func);
+            print_sym(vm, l, *lastip, &last_func);
             goto check_time;
         }
         if (lastip.has_value()) {
@@ -186,28 +184,30 @@ static void replay(shared_ptr<memory_view> vm, nexusrv_msg_decoder *msg_decoder,
                 assert(rc < 0);
                 goto handle_error;
             }
-            align_print(&addr_printed, fp, fprintf(fp,
+            l.newline();
+            align_print(&addr_printed, l, l.format(
                         FMT_TIME_OFFSET " 0x%" PRIx64 ",+%" PRIu32 "  ",
                         nexusrv_trace_time(&trace_decoder),
                         nexusrv_msg_decoder_offset(msg_decoder),
                         instblock->addr, instblock->icnt));
             if (event == NEXUSRV_Trace_Event_None) {
-                align_print(&inst_printed, fp, instblock->print(fp));
+                align_print(&inst_printed, l,
+                    l.print(instblock->to_string().c_str()));
                 // Indent with stack depth
-                fprintf(fp, " │ %*s", stack, "");
-                print_sym(vm, fp, instblock->addr, &last_func);
+                l.format(" │ %*s", stack, "");
+                print_sym(vm, l, instblock->addr, &last_func);
                 continue;
             }
             auto insn = rv_inst_block::disasm1(vm, *lastip, true);
-            align_print(&inst_printed, fp, fprintf(
-                    fp, "[retired %" PRIu64 "] %s%s%s",
+            align_print(&inst_printed, l, l.format(
+                    "[retired %" PRIu64 "] %s%s%s",
                     (*lastip - instblock->addr) / 2,
                     insn ? insn->mnemonic : "",
                     insn ? " " : "",
                     insn ? insn->op_str : ""));
             goto handle_event;
-        } else
-            rc = nexusrv_trace_try_retire(&trace_decoder, UINT32_MAX, &event);
+        }
+        rc = nexusrv_trace_try_retire(&trace_decoder, UINT32_MAX, &event);
 handle_error:
         if (rc < 0) switch (rc) {
             case -nexus_trace_eof:
@@ -215,6 +215,7 @@ handle_error:
             case -nexus_msg_unsupported:
                 goto unknown_msg;
             default:
+                l.flush();
                 error(-rc, 0, "try_retire failed: %s",
                       str_nexus_error(-rc));
         }
@@ -222,7 +223,8 @@ handle_error:
             lastip.emplace(*lastip + (uint32_t)rc * 2);
         if (rc) {
             tnt_time = 0;
-            fprintf(fp,FMT_TIME_OFFSET "I-CNT %" PRIi32,
+            l.newline();
+            l.format(FMT_TIME_OFFSET "I-CNT %" PRIi32,
                     nexusrv_trace_time(&trace_decoder),
                     nexusrv_msg_decoder_offset(msg_decoder),
                     rc);
@@ -233,35 +235,42 @@ handle_event:
             case NEXUSRV_Trace_Event_DirectSync: {
                 /* Have no way to tell the branch target, reset lastip */
                 lastip.reset();
-                if (tnt_time != nexusrv_trace_time(&trace_decoder))
-                    fprintf(fp,FMT_TIME_OFFSET "TNT ",
+                if (tnt_time != nexusrv_trace_time(&trace_decoder)) {
+                    l.newline();
+                    l.format(FMT_TIME_OFFSET "TNT ",
                             nexusrv_trace_time(&trace_decoder),
                             nexusrv_msg_decoder_offset(msg_decoder));
+                }
                 tnt_time = nexusrv_trace_time(&trace_decoder);
                 rc = nexusrv_trace_next_tnt(&trace_decoder);
-                if (rc < 0)
+                if (rc < 0) {
+                    l.flush();
                     error(-rc, 0, "next_tnt failed: %s",
                           str_nexus_error(-rc));
-                fprintf(fp, "%c", rc > 0 ? '!' : '.');
+                }
+                l.format("%c", rc > 0 ? '!' : '.');
                 break;
             }
             case NEXUSRV_Trace_Event_Indirect:
             case NEXUSRV_Trace_Event_IndirectSync:
             case NEXUSRV_Trace_Event_Trap: {
                 rc = nexusrv_trace_next_indirect(&trace_decoder, &indir);
-                if (rc < 0)
+                if (rc < 0) {
+                    l.flush();
                     error(-rc, 0, "next_indir failed: %s",
                           str_nexus_error(-rc));
+                }
                 lastip.emplace(indir.target);
-                fprintf(fp,FMT_TIME_OFFSET "INDIRECT%s%s to 0x%" PRIx64,
+                l.newline();
+                l.format(FMT_TIME_OFFSET "INDIRECT%s%s to 0x%" PRIx64,
                         nexusrv_trace_time(&trace_decoder),
                         nexusrv_msg_decoder_offset(msg_decoder),
                         indir.interrupt ? " interrupt" : "",
                         indir.exception ? " exception" : "",
                         *lastip);
-                print_sym(vm, fp, indir.target, &last_func);
+                print_sym(vm, l, indir.target, &last_func);
                 if (indir.ownership)
-                    fprintf(fp, " fmt=%u priv=%u v=%u context=0x%" PRIx64,
+                    l.format(" fmt=%u priv=%u v=%u context=0x%" PRIx64,
                             indir.ownership_fmt,
                             indir.ownership_priv,
                             indir.ownership_v,
@@ -270,24 +279,30 @@ handle_event:
             }
             case NEXUSRV_Trace_Event_Sync: {
                 rc = nexusrv_trace_next_sync(&trace_decoder, &sync);
-                if (rc < 0)
+                if (rc < 0) {
+                    l.flush();
                     error(-rc, 0, "next_sync failed: %s",
                           str_nexus_error(-rc));
+                }
                 lastip.emplace(sync.addr);
-                fprintf(fp,FMT_TIME_OFFSET "SYNC %u to 0x%" PRIx64,
+                l.newline();
+                l.format(FMT_TIME_OFFSET "SYNC %u to 0x%" PRIx64,
                         nexusrv_trace_time(&trace_decoder),
                         nexusrv_msg_decoder_offset(msg_decoder),
                         sync.sync, *lastip);
-                print_sym(vm, fp, sync.addr, &last_func);
+                print_sym(vm, l, sync.addr, &last_func);
                 break;
             }
             case NEXUSRV_Trace_Event_Stop: {
                 lastip.reset();
                 rc = nexusrv_trace_next_stop(&trace_decoder, &stop);
-                if (rc < 0)
+                if (rc < 0) {
+                    l.flush();
                     error(-rc, 0, "next_stop failed: %s",
                           str_nexus_error(-rc));
-                fprintf(fp, FMT_TIME_OFFSET "STOP evcode=%u",
+                }
+                l.newline();
+                l.format(FMT_TIME_OFFSET "STOP evcode=%u",
                         nexusrv_trace_time(&trace_decoder),
                         nexusrv_msg_decoder_offset(msg_decoder),
                         stop.evcode);
@@ -296,10 +311,13 @@ handle_event:
             case NEXUSRV_Trace_Event_Error: {
                 lastip.reset();
                 rc = nexusrv_trace_next_error(&trace_decoder, &err);
-                if (rc < 0)
+                if (rc < 0) {
+                    l.flush();
                     error(-rc, 0, "next_error failed: %s",
                           str_nexus_error(-rc));
-                fprintf(fp, FMT_TIME_OFFSET "ERROR etype=%u ecode=%u",
+                }
+                l.newline();
+                l.format(FMT_TIME_OFFSET "ERROR etype=%u ecode=%u",
                         nexusrv_trace_time(&trace_decoder),
                         nexusrv_msg_decoder_offset(msg_decoder),
                         err.etype, err.ecode);
@@ -308,6 +326,7 @@ handle_event:
         }
 check_time:
         if (last_time && last_time > nexusrv_trace_time(&trace_decoder)) {
+            l.flush();
             error(0, 0, "WARN: Time goes backward, %" PRIu64 " vs %" PRIu64,
                   last_time, nexusrv_trace_time(&trace_decoder));
         }
@@ -315,13 +334,17 @@ check_time:
         continue;
 unknown_msg:
         rc = nexusrv_msg_decoder_next(msg_decoder, &msg);
-        if (rc < 0)
+        if (rc < 0) {
+            l.flush();
             error(-rc, 0, "msg_decoder_next failed: %s",
                   str_nexus_error(-rc));
+        }
         assert(rc > 0);
         nexusrv_trace_add_timestamp(&trace_decoder, msg.timestamp);
-        fprintf(fp, "\n[%" PRIu64 "] UNKNOWN MSG ", nexusrv_trace_time(&trace_decoder));
+        l.flush();
+        fprintf(fp, "[%" PRIu64 "] UNKNOWN MSG ", nexusrv_trace_time(&trace_decoder));
         nexusrv_print_msg(fp, &msg);
+        fputc('\n', fp);
     }
 done_trace:
     nexusrv_trace_decoder_fini(&trace_decoder);
